830b301c36bf2168a8f6450306e40df494f67e54
Aaron Cruz <pferdefleisch+git@gmail.com>
2018-10-26 10:54:54 +0300

    Don't allow bad trx's in blockchain

--- blockchain/blockchain.rb
+++ blockchain/blockchain.rb
@@ -2,39 +2,51 @@ class BlockChain
   attr_reader :blocks
   attr_accessor :target
 
+  def self.load(serialized)
+    Marshal.load(serialized)
+  end
+
   def initialize(wallet, initial_target=2, start_amount=10_000_000)
     @target = initial_target
     @blocks = []
     @blocks << Block.create_genesis_block(wallet.genesis_trx(start_amount), initial_target)
   end
 
-  def length
+  def height
     @blocks.length
   end
 
   def add_to_chain(trx)
-    @blocks << Block.new(@blocks.last, trx, @target)
+    block = Block.new(@blocks.last, trx, @target)
+    raise ArgumentError, "Invalid trx" unless all_spends_valid?(@blocks.dup << block)
+    @blocks << block
+    puts block.to_s
+    block
+  end
+
+  def marshal
+    Marshal.dump(self)
   end
 
   def valid?
     @blocks.all? { |block| block.is_a?(Block) } &&
       @blocks.all?(&:valid?) &&
       @blocks.each_cons(2).all? { |a, b| a.own_hash == b.prev_block_hash } &&
-      all_spends_valid?
+      all_spends_valid?(@blocks.dup)
   end
 
-  def all_spends_valid?
-    compute_balances do |balances, from, to|
+  def all_spends_valid?(blocks)
+    compute_balances(blocks) do |balances, from, to|
       return false if balances.values_at(from, to).any? { |bal| bal < 0 }
     end
     true
   end
 
-  def compute_balances
-    genesis_trx = @blocks.first.trx
+  def compute_balances(blocks)
+    genesis_trx = blocks.first.trx
     balances = { genesis_trx.to => genesis_trx.amount }
     balances.default = 0 # New people automatically have balance of 0
-    @blocks.drop(1).each do |block| # Ignore the genesis block
+    blocks.drop(1).each do |block| # Ignore the genesis block
       from = block.trx.from
       to = block.trx.to
       amount = block.trx.amount
